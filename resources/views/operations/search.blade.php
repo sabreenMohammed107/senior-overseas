340dd03e39939e03d52299a544b045d521b96867
sabreenMohammed107 <sabreenm312@gmail.com>
2020-07-03 10:45:52 +0200

    ffd

--- resources/views/operations/search.blade.php
+++ resources/views/operations/search.blade.php
@@ -196,7 +196,7 @@
 					<td>{{$filter->ocean->notes}}</td>
 					@endif
 					<td>
-						<?php echo $trackings[$index]->car_price; ?>
+						{{$filter->price}}
 					</td>
 					<td scope="col">
 						@if($typeTesting==0)
