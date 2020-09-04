0075573487a5aaceb2cc85c08c9ea0bcc85c92b4
sabreenMohammed107 <sabreenm312@gmail.com>
2020-09-04 22:20:02 +0200

    nn

--- app/Http/Controllers/ClientReport.php
+++ app/Http/Controllers/ClientReport.php
@@ -267,7 +267,7 @@ class ClientReport extends Controller
         $filtterTotal = $filtters->get();
         $filtter1 =$filtters->where('operation_id',null)->get();
         $filtterss = $filtters->whereNotNull('operation_id')->get()->groupBy('operation_id');
-        $operationIds=$filtters->pluck('operation_id');
+        $operationIds=$filtters->get('operation_id');
         $test=array();
 dd($operationIds);
                 //-----------------New----------------//
