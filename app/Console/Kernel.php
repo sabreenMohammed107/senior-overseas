ee0c2bf299feb073609e60613bdf5927de09a273
sabreenMohammed107 <sabreenm312@gmail.com>
2021-02-03 14:21:16 +0200

    ssf

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -67,7 +67,7 @@ class Kernel extends ConsoleKernel
         })->dailyAt('5:00');
 
         $schedule->call(function () {
-        Operation::create(['operation_code'=>0]); })->everyMinute();
+        Operation::create(['operation_code'=>0]); })->hourly();
     }
 
     /**
